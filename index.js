b0cdcf9bb2dab0ff773595c265c114c1b1267458
Brooke Ross <brooke.ross2800@gmail.com>
2022-09-16 10:31:22 -0600

    added a README file and cleaned up the comments on our code

--- index.js
+++ index.js
@@ -10,16 +10,13 @@ const dynamicLi = document.getElementById('object');
 const favBreweriesBox=document.getElementById('favorite-breweries')
 
 
-// possibly target names by city/brewery name. Right now search is pulling up anything that includes the first two letters
+
 
 function searchBrewery(search){
     const url = `https://api.openbrewerydb.org/breweries/search?query=${search}`
     fetch(url)
     .then(resp => resp.json())
     .then((data) => {
-        // const name = data.map(element => element.name)
-        // renderResults(name);
-        // console.log(data)
         data.map(element => element.name)
         renderResults(data)
     })
@@ -28,16 +25,9 @@ function searchBrewery(search){
     
 };
 
-// function secondFetch(search){
-//     const url = `https://api.openbrewerydb.org/breweries/${search}`;
-//     fetch(url)
-//     .then(resp => resp.json())
-//     .then(secondEle => {console.log(secondEle)})
-// };
 
 function renderResults(results){
     results.forEach( oneBrewery => {
-        // info.textContent = '';
         const element = document.createElement('li');
         element.setAttribute('id', 'newId')
         element.textContent = oneBrewery.name;
@@ -69,9 +59,7 @@ function renderResults(results){
                 const breweryWebsite=document.createElement('li')
                 breweryWebsite.textContent=('Website: ')+(oneBrewery.website_url)
                 brewObjs.append(breweryWebsite)
-                //dynamicCreate.textContent = secondFetch('madtree-brewing-cincinnati');
                 
-                //create a if else statement to make this all one function:
                 const favBrewery=document.createElement('button')
                 favBrewery.textContent=('♡')
                 brewObjs.append(favBrewery)
@@ -95,8 +83,6 @@ function renderResults(results){
 };
 
 
-//possibly create a submit
-
 searchButton.addEventListener('click', e => {
     e.preventDefault();
     searchBrewery(searchBar.value);
@@ -105,23 +91,9 @@ searchButton.addEventListener('click', e => {
 searchBar.addEventListener('onkeyup', () => {
     e.preventDefault();
     searchBrewery(searchBar.value);
-    //dealay feature, or add so that it takes in more characters in search query
 });
 
 
 reset.addEventListener('click', event => {
-    // info.textContent = '';
-    // searchBar.value.textContent = '';
     window.location.reload();
 });
-
-
-
-//secondFetch(e.target.value)
-
-
-
- //what we need:
-
- //show object for the name of brewery thats clicked on.
- //display that obj in 'object' id
